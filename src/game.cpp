bf2e1be2fe23bcc2249a7884c8f95d93fe5a55a0
Tyler K <tyly04@gmail.com>
2024-04-16 11:19:39 -0700

    Added down controls

--- src/game.cpp
+++ src/game.cpp
@@ -6,11 +6,15 @@ using namespace std;
 SDL_Rect player;
 Controller player_controller;
 shared_ptr<Control> up;
+shared_ptr<Control> down;
 
 void update(App& app) {
 	player_controller.update();
-	if (up->getValue().data.boolean == true) {
+	if (down->getValue().data.boolean == true) {
 		player.y += 1;
+	}
+	if (up->getValue().data.boolean == true) {
+		player.y -= 1;
 	}
 	SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 0xff);
 	SDL_RenderClear(app.renderer);
@@ -23,7 +27,9 @@ ExitCode game(App& app) {
 	player = SDL_Rect{10, 10, 10, 10};
 	player_controller = Controller {};
 	player_controller.bindControl("up", BOOL, 2, KeySource("W"), KeySource("Up"));
+	player_controller.bindControl("down", BOOL, 2, KeySource("S"), KeySource("Down"));
 	// player_controller.listenForControl("up", Event<ControlDataOut>{upPress});
 	up = player_controller.getControl("up");
+	down = player_controller.getControl("down");
 	return app.update(update);
 }
